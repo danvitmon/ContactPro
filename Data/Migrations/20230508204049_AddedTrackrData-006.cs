0e7682e6d9b2fa592cfeea91b44140b64f24daf4
Daniel Monastirsky <danvitmon@gmail.com>
2023-05-08 15:03:00 -0700

    initial

---
+++ Data/Migrations/20230508204049_AddedTrackrData-006.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactPro.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTrackrData006 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Contacts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Contacts",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Contacts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Contacts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Data/Migrations/20230508205732_AddedCorrectInfo-007.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactPro.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCorrectInfo007 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address1",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Address2",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Contacts",
+                newName: "DueDate");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Contacts",
+                newName: "Complete");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Contacts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DueDate",
+                table: "Contacts",
+                newName: "DateOfBirth");
+
+            migrationBuilder.RenameColumn(
+                name: "Complete",
+                table: "Contacts",
+                newName: "City");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Contacts",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address1",
+                table: "Contacts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address2",
+                table: "Contacts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Contacts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Contacts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Contacts",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Contacts",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ZipCode",
+                table: "Contacts",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
